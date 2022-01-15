e9a47d7d04b1166df4cd85ecccaa0d88483a0d44
Sabio <jasmine.sabio@gmail.com>
2022-01-16 01:33:35 +0800

    front end

---
+++ frontend/hello-world/src/App.js
@@ -0,0 +1,55 @@
+// import Web3 from 'web3';
+import React, { useEffect, useState, Component } from "react";
+// import myNft from './utils/MyNft.json';
+
+const App = () => {
+
+  const checkIfWalletIsConnected = () => {
+    /*
+    * First make sure we have access to window.ethereum
+    */
+    const { ethereum } = window;
+
+    if (!ethereum) {
+      console.log("Make sure you have metamask!");
+      return;
+    } else {
+      console.log("We have the ethereum object", ethereum);
+    }
+  }
+
+  // Render Methods
+  const renderNotConnectedContainer = () => (
+    <button className="cta-button connect-wallet-button">
+      Connect to Wallet
+    </button>
+  );
+
+  /*
+  * This runs our function when the page loads.
+  */
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, [])
+
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="header-container">
+          <p className="header gradient-text">My NFT Collection</p>
+          <p className="sub-text">
+            Each unique. Each beautiful. Discover your NFT today.
+          </p>
+          {/* Add your render method here */}
+          {renderNotConnectedContainer()}
+        </div>
+        <div className="footer-container">
+          
+          
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
